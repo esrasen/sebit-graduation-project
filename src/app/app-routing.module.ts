c43b7b7f9a8e6195be19657c2a78af0c30005424
Esra Sen <sen.esra@yahoo.com>
2022-08-28 17:33:13 +0300

    Add search component mechanic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './components/category/category.component';
 import { HomeComponent } from './components/home/home.component';
 import { PostDetailComponent } from './components/post-detail/post-detail.component';
+import { SearchComponent } from './components/search/search.component';
 
 const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: 'home'},
   {path:'home', component: HomeComponent},
   {path:'home/category/:categoryId', component:CategoryComponent},
-  {path:'home/post/:postId', component:PostDetailComponent},
+  {path:'post/:postId', component:PostDetailComponent},
+  {path:'search/:searchTerm',component:SearchComponent},
 
 ];
 
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  postData:any;
+
+  @Input() max: Number = 280;
+
+  readMore:boolean = false;
+  constructor(
+    private route:ActivatedRoute,
+    private postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe((params:any) => {
+      this.postService.getPosts().subscribe(res => {
+        if(params.searchTerm)
+        {
+          this.postData = res.filter((post:any)=>
+          post.description.toLowerCase().includes(params.searchTerm.toLowerCase()) |
+          post.title.toLowerCase().includes(params.searchTerm.toLowerCase())
+          )
+        }
+        else
+        {
+          this.postService.getPosts().subscribe((res)=>{
+            this.postData = res;
+          })
+        }
+      })
+    })
   }
 
 }

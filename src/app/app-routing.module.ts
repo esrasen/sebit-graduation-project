5abba078b0e08c2a08da68adf533453e576a41de
Esra Sen <sen.esra@yahoo.com>
2022-08-28 14:35:54 +0300

    Add category

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoryComponent } from './components/category/category.component';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {path:'', pathMatch: 'full', redirectTo: 'home'},
   {path:'home', component: HomeComponent},
+  {path:'home/category/:categoryId', component:CategoryComponent},
+
 ];
 
 @NgModule({
--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CategoryService } from 'src/app/services/category.service';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-category',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryComponent implements OnInit {
 
-  constructor() { }
+  categoryId:any;
+  categoryObj:any;
+  postData:any;
+  totalPost:any;
+
+  @Input() max: number = 280;
+
+  readMore: boolean = false;
+
+  constructor(
+    private categoryService: CategoryService,
+    private postService:PostService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.categoryId = this.route.snapshot.paramMap.get('categoryId');
+
+    this.categoryService.getCategory(this.categoryId).subscribe((res)=>{
+      this.categoryObj = res;
+    })
+
+    this.postService.getPosts().subscribe((res)=>{
+      this.postData = res.filter((x: {category_id: any}) =>x.category_id == this.categoryId)
+      .sort((a: any, b:any)=>{ return b.date > a.date ? 1 : b.date < a.date ? -1 : 0});
+
+      this.totalPost = Object.keys(this.postData).length;
+    })
   }
 
 }
